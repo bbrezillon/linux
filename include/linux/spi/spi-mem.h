f71dfc442d8e91204819653fe6a15c70b02e4d2a
Boris Brezillon <boris.brezillon@bootlin.com>
2018-09-18 10:52:18 +0200

    spi: spi-mem: Prepare things for dual bytes opcodes support
    Some SPI NORs are using 2bytes opcodes. Make opcode an u16 and add an
    nbytes field to specify the number of opcode bytes.
    
    Also add the SPI_MEM_OP_CMD_16B() to declare 2bytes opcodes.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>

--- include/linux/spi/spi-mem.h
+++ include/linux/spi/spi-mem.h
@@ -17,6 +17,14 @@
 	{							\
 		.buswidth = __buswidth,				\
 		.opcode = __opcode,				\
+		.nbytes = 1,					\
+	}
+
+#define SPI_MEM_OP_CMD_16B(__opcode, __buswidth)		\
+	{							\
+		.buswidth = __buswidth,				\
+		.opcode = __opcode,				\
+		.nbytes = 2,					\
 	}
 
 #define SPI_MEM_OP_ADDR(__nbytes, __val, __buswidth)		\
@@ -67,6 +75,7 @@ enum spi_mem_data_dir {
 
 /**
  * struct spi_mem_op - describes a SPI memory operation
+ * @cmd.nbytes: number of opcode bytes (only 1 or 2 are valid)
  * @cmd.buswidth: number of IO lines used to transmit the command
  * @cmd.dtr: set true to transfer opcode in double transfer rate mode
  * @cmd.opcode: operation opcode
@@ -92,9 +101,10 @@ enum spi_mem_data_dir {
  */
 struct spi_mem_op {
 	struct {
+		u8 nbytes;
 		u8 buswidth;
 		bool dtr;
-		u8 opcode;
+		u16 opcode;
 	} cmd;
 
 	struct {
