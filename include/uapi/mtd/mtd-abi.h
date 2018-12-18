378c9db1478dc6501c9a8b16c655297c6322a0bf
Boris Brezillon <boris.brezillon@bootlin.com>
2018-12-19 00:36:53 +0100

    mtd: Add support for emulated SLC mode on MLC NANDs
    MLC NANDs can be made a bit more reliable if we only program the lower
    page of each pair. At least, this solves the paired-pages corruption
    issue.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>

--- include/uapi/mtd/mtd-abi.h
+++ include/uapi/mtd/mtd-abi.h
@@ -104,6 +104,7 @@ struct mtd_write_req {
 #define MTD_BIT_WRITEABLE	0x800	/* Single bits can be flipped */
 #define MTD_NO_ERASE		0x1000	/* No erase necessary */
 #define MTD_POWERUP_LOCK	0x2000	/* Always locked after reset */
+#define MTD_MLC_IN_SLC_MODE	0x4000	/* Emulate SLC behavior on MLC NANDs */
 
 /* Some common devices / combinations of capabilities */
 #define MTD_CAP_ROM		0
