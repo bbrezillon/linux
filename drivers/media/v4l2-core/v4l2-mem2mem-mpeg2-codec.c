00874b76cb8aad10d0760077a55797303601fa50
Boris Brezillon <boris.brezillon@collabora.com>
2019-06-07 10:41:58 +0200

    media: v4l2: Add MPEG2 codec helpers
    Provide generic helpers for mem2mem MPEG2 codec drivers.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

---
+++ drivers/media/v4l2-core/v4l2-mem2mem-mpeg2-codec.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Memory-to-memory MPEG2 codec framework for Video for Linux 2.
+ *
+ * Helper functions for MPEG2 codec devices that use memory buffers for both
+ * source and destination.
+ *
+ * Copyright (c) 2019 Collabora Ltd.
+ *
+ * Author:
+ *      Boris Brezillon <boris.brezillon@collabora.com>
+ */
+
+#include <linux/types.h>
+#include <media/v4l2-mem2mem-mpeg2-codec.h>
+
+void v4l2_m2m_mpeg2_codec_run_preamble(struct v4l2_m2m_codec_ctx *ctx,
+				       struct v4l2_m2m_mpeg2_codec_run *run)
+{
+	struct v4l2_ctrl *ctrl;
+
+	ctrl = v4l2_ctrl_find(&ctx->ctrl_hdl,
+			      V4L2_CID_MPEG_VIDEO_MPEG2_SLICE_PARAMS);
+	run->slice_params = ctrl ? ctrl->p_cur.p : NULL;
+	ctrl = v4l2_ctrl_find(&ctx->ctrl_hdl,
+			      V4L2_CID_MPEG_VIDEO_MPEG2_QUANTIZATION);
+	run->quantization = ctrl ? ctrl->p_cur.p : NULL;
+
+	v4l2_m2m_codec_run_preamble(ctx, &run->base);
+}
+EXPORT_SYMBOL_GPL(v4l2_m2m_mpeg2_codec_run_preamble);
---
+++ include/media/v4l2-mem2mem-mpeg2-codec.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Memory-to-memory MPEG2 codec framework for Video for Linux 2.
+ *
+ * Helper functions for MPEG2 codec devices that use memory buffers for both
+ * source and destination.
+ *
+ * Copyright (c) 2019 Collabora Ltd.
+ *
+ * Author:
+ *	Boris Brezillon <boris.brezillon@collabora.com>
+ */
+
+#ifndef _MEDIA_V4L2_MEM2MEM_MPEG2_CODEC_H
+#define _MEDIA_V4L2_MEM2MEM_MPEG2_CODEC_H
+
+#include <media/mpeg2-ctrls.h>
+#include <media/v4l2-mem2mem-codec.h>
+
+struct v4l2_m2m_mpeg2_codec_run {
+	struct v4l2_m2m_codec_run base;
+	const struct v4l2_ctrl_mpeg2_slice_params *slice_params;
+	const struct v4l2_ctrl_mpeg2_quantization *quantization;
+};
+
+void v4l2_m2m_mpeg2_codec_run_preamble(struct v4l2_m2m_codec_ctx *ctx,
+				       struct v4l2_m2m_mpeg2_codec_run *run);
+void v4l2_m2m_mpeg2_codec_run_postamble(struct v4l2_m2m_codec_ctx *ctx,
+					struct v4l2_m2m_mpeg2_codec_run *run);
+
+#endif /* _MEDIA_V4L2_MEM2MEM_CODEC_H */
