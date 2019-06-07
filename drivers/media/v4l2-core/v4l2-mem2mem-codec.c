a05520a60224c207d4abe4321a46a11fe1f3e8b5
Boris Brezillon <boris.brezillon@collabora.com>
2019-06-07 16:52:38 +0200

    fixup! media: v4l2: Add m2m codec helpers

--- drivers/media/v4l2-core/v4l2-mem2mem-codec.c
+++ drivers/media/v4l2-core/v4l2-mem2mem-codec.c
@@ -11,7 +11,73 @@
  *      Boris Brezillon <boris.brezillon@collabora.com>
  */
 
+#include <media/v4l2-dev.h>
 #include <media/v4l2-mem2mem-codec.h>
+
+int v4l2_m2m_codec_ctrls_init(struct v4l2_m2m_codec_ctx *ctx,
+			      const struct v4l2_ctrl_config *ctrls,
+			      unsigned int num_ctrls)
+{
+	unsigned int i;
+	int ret;
+
+	for (i = 0; i < num_ctrls; i++) {
+		const struct v4l2_ctrl_config *cfg = &ctrls[i];
+
+		if (cfg->type == V4L2_CTRL_TYPE_MENU ||
+		    cfg->type == V4L2_CTRL_TYPE_INTEGER_MENU)
+			v4l2_ctrl_new_std_menu(&ctx->ctrl_hdl,
+					       cfg->ops, cfg->id, cfg->max,
+					       cfg->menu_skip_mask, cfg->def);
+		else if (cfg->type > V4L2_CTRL_COMPOUND_TYPES)
+			v4l2_ctrl_new_custom(&ctx->ctrl_hdl, cfg, ctx);
+		else
+			v4l2_ctrl_new_std(&ctx->ctrl_hdl, cfg->ops, cfg->id,
+					  cfg->min, cfg->max, cfg->step,
+					  cfg->def);
+
+		if (ctx->ctrl_hdl.error) {
+			ret = ctx->ctrl_hdl.error;
+			goto err_free_handler;
+		}
+	}
+
+	ret = v4l2_ctrl_handler_setup(&ctx->ctrl_hdl);
+	if (ret)
+		goto err_free_handler;
+
+	return 0;
+
+err_free_handler:
+	v4l2_ctrl_handler_free(&ctx->ctrl_hdl);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(v4l2_m2m_codec_ctrls_init);
+
+void v4l2_m2m_codec_ctrls_cleanup(struct v4l2_m2m_codec_ctx *ctx)
+{
+	v4l2_ctrl_handler_free(&ctx->ctrl_hdl);
+}
+EXPORT_SYMBOL_GPL(v4l2_m2m_codec_ctrls_cleanup);
+
+void v4l2_m2m_codec_open(struct file *file,
+			 struct v4l2_m2m_dev *m2m_dev,
+			 struct v4l2_m2m_codec_ctx *ctx)
+{
+	ctx->m2m_dev = m2m_dev;
+
+	v4l2_fh_init(&ctx->fh, video_devdata(file));
+	file->private_data = &ctx->fh;
+	v4l2_fh_add(&ctx->fh);
+}
+EXPORT_SYMBOL_GPL(v4l2_m2m_codec_open);
+
+void v4l2_m2m_codec_release(struct v4l2_m2m_codec_ctx *ctx)
+{
+	v4l2_fh_del(&ctx->fh);
+	v4l2_fh_exit(&ctx->fh);
+}
+EXPORT_SYMBOL_GPL(v4l2_m2m_codec_release);
 
 void v4l2_m2m_codec_run_preamble(struct v4l2_m2m_codec_ctx *ctx,
 				 struct v4l2_m2m_codec_run *run)
@@ -42,8 +108,7 @@ void v4l2_m2m_codec_run_postamble(struct v4l2_m2m_codec_ctx *ctx,
 }
 EXPORT_SYMBOL_GPL(v4l2_m2m_codec_run_postamble);
 
-void v4l2_m2m_codec_job_finish(struct v4l2_m2m_dev *m2m_dev,
-			       struct v4l2_m2m_codec_ctx *ctx,
+void v4l2_m2m_codec_job_finish(struct v4l2_m2m_codec_ctx *ctx,
 			       enum vb2_buffer_state state)
 {
 	struct vb2_v4l2_buffer *src_buf, *dst_buf;
@@ -58,6 +123,6 @@ void v4l2_m2m_codec_job_finish(struct v4l2_m2m_dev *m2m_dev,
 	if (dst_buf)
 		v4l2_m2m_buf_done(dst_buf, state);
 
-        v4l2_m2m_job_finish(m2m_dev, ctx->fh.m2m_ctx);
+        v4l2_m2m_job_finish(ctx->m2m_dev, ctx->fh.m2m_ctx);
 }
 EXPORT_SYMBOL_GPL(v4l2_m2m_codec_job_finish);
