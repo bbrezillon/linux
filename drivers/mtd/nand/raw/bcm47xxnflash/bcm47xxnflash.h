92b195aa4a50bd673ab2ff6fb89d3d1f03507dab
Boris Brezillon <boris.brezillon@collabora.com>
2020-04-19 14:19:09 +0200

    mtd: rawnand: bcm47xx: Get rid of the legacy implementation
    Now that exec_op() is implemented we don't need to implement all those
    legacy hooks, which simplifies the code quite a bit.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Miquel Raynal <miquel.raynal@bootlin.com>
    ---
    Changes in v2:
    * Add R-b

--- drivers/mtd/nand/raw/bcm47xxnflash/bcm47xxnflash.h
+++ drivers/mtd/nand/raw/bcm47xxnflash/bcm47xxnflash.h
@@ -14,12 +14,6 @@ struct bcm47xxnflash {
 	struct bcma_drv_cc *cc;
 
 	struct nand_chip nand_chip;
-
-	unsigned curr_command;
-	int curr_page_addr;
-	int curr_column;
-
-	u8 id_data[8];
 };
 
 int bcm47xxnflash_ops_bcm4706_init(struct bcm47xxnflash *b47n);
