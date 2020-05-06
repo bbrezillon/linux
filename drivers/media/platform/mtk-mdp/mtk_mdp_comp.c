1501116c4de02fa2376d357e527208aafb5db7ac
Eizan Miyamoto <eizan@chromium.org>
2020-05-06 18:40:38 +1000

    FROMLIST: mtk-mdp: add driver to probe mdp components
    Broadly, this patch (1) adds a driver for various MTK MDP components to
    go alongside the main MTK MDP driver, and (2) hooks them all together
    using the component framework.
    
    (1) Up until now, the MTK MDP driver controls 8 devices in the device
    tree on its own. When running tests for the hardware video decoder, we
    found that the iommus and LARBs were not being properly configured. To
    configure them, a driver for each be added to mtk_mdp_comp so that
    mtk_iommu_add_device() can (eventually) be called from dma_configure()
    inside really_probe().
    
    (2) The integration into the component framework allows us to defer the
    registration with the v4l2 subsystem until all the MDP-related devices
    have been probed, so that the relevant device node does not become
    available until initialization of all the components is complete.
    
    Some notes about how the component framework has been integrated:
    
    - The driver for the rdma0 component serves double duty as the "master"
      (aggregate) driver as well as a component driver. This is a non-ideal
      compromise until a better solution is developed. This device is
      differentiated from the rest by checking for a "mediatek,vpu" property
      in the device node.
    
    - The list of mdp components remains hard-coded as mtk_mdp_comp_dt_ids[]
      in mtk_mdp_core.c, and as mtk_mdp_comp_driver_dt_match[] in
      mtk_mdp_comp.c. This unfortunate duplication of information is
      addressed in a following patch in this series.
    
    - The component driver calls component_add() for each device that is
      probed.
    
    - In mtk_mdp_probe (the "master" device), we scan the device tree for
      any matching nodes against mtk_mdp_comp_dt_ids, and add component
      matches for them. The match criteria is a matching device node
      pointer.
    
    - When the set of components devices that have been probed corresponds
      with the list that is generated by the "master", the callback to
      mtk_mdp_master_bind() is made, which then calls the component bind
      functions.
    
    - Inside mtk_mdp_master_bind(), once all the component bind functions
      have been called, we can then register our device to the v4l2
      subsystem.
    
    - The call to pm_runtime_enable() in the master device is called after
      all the components have been registered by their bind() functions
      called by mtk_mtp_master_bind(). As a result, the list of components
      will not change while power management callbacks mtk_mdp_suspend()/
      resume() are accessing the list of components.
    
    Signed-off-by: Eizan Miyamoto <eizan@chromium.org>

--- drivers/media/platform/mtk-mdp/mtk_mdp_comp.c
+++ drivers/media/platform/mtk-mdp/mtk_mdp_comp.c
@@ -5,14 +5,53 @@
  */
 
 #include <linux/clk.h>
+#include <linux/component.h>
 #include <linux/device.h>
-#include <linux/of.h>
+#include <linux/module.h>
 #include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/of.h>
+#include <linux/of_irq.h>
 #include <linux/of_platform.h>
 #include <soc/mediatek/smi.h>
+#include <linux/platform_device.h>
 
 #include "mtk_mdp_comp.h"
+#include "mtk_mdp_core.h"
 
+/**
+ * enum mtk_mdp_comp_type - the MDP component
+ * @MTK_MDP_RDMA:		Read DMA
+ * @MTK_MDP_RSZ:		Reszer
+ * @MTK_MDP_WDMA:		Write DMA
+ * @MTK_MDP_WROT:		Write DMA with rotation
+ * @MTK_MDP_COMP_TYPE_MAX:	Placeholder for num elems in this enum
+ */
+enum mtk_mdp_comp_type {
+	MTK_MDP_RDMA,
+	MTK_MDP_RSZ,
+	MTK_MDP_WDMA,
+	MTK_MDP_WROT,
+	MTK_MDP_COMP_TYPE_MAX,
+};
+
+static const struct of_device_id mtk_mdp_comp_driver_dt_match[] = {
+	{
+		.compatible = "mediatek,mt8173-mdp-rdma",
+		.data = (void *)MTK_MDP_RDMA
+	}, {
+		.compatible = "mediatek,mt8173-mdp-rsz",
+		.data = (void *)MTK_MDP_RSZ
+	}, {
+		.compatible = "mediatek,mt8173-mdp-wdma",
+		.data = (void *)MTK_MDP_WDMA
+	}, {
+		.compatible = "mediatek,mt8173-mdp-wrot",
+		.data = (void *)MTK_MDP_WROT
+	},
+	{ }
+};
+MODULE_DEVICE_TABLE(of, mtk_mdp_comp_driver_dt_match);
 
 void mtk_mdp_comp_clock_on(struct device *dev, struct mtk_mdp_comp *comp)
 {
@@ -20,10 +59,14 @@ void mtk_mdp_comp_clock_on(struct device *dev, struct mtk_mdp_comp *comp)
 
 	if (comp->larb_dev) {
 		err = mtk_smi_larb_get(comp->larb_dev);
-		if (err)
+		if (err) {
+			enum mtk_mdp_comp_type comp_type =
+				(enum mtk_mdp_comp_type)
+				of_device_get_match_data(dev);
 			dev_err(dev,
 				"failed to get larb, err %d. type:%d\n",
-				err, comp->type);
+				err, comp_type);
+		}
 	}
 
 	for (i = 0; i < ARRAY_SIZE(comp->clk); i++) {
@@ -32,8 +75,8 @@ void mtk_mdp_comp_clock_on(struct device *dev, struct mtk_mdp_comp *comp)
 		err = clk_prepare_enable(comp->clk[i]);
 		if (err)
 			dev_err(dev,
-			"failed to enable clock, err %d. type:%d i:%d\n",
-				err, comp->type, i);
+				"failed to enable clock, err %d. i:%d\n",
+				err, i);
 	}
 }
 
@@ -51,16 +94,41 @@ void mtk_mdp_comp_clock_off(struct device *dev, struct mtk_mdp_comp *comp)
 		mtk_smi_larb_put(comp->larb_dev);
 }
 
-int mtk_mdp_comp_init(struct device *dev, struct device_node *node,
-		      struct mtk_mdp_comp *comp,
-		      enum mtk_mdp_comp_type comp_type)
+static int mtk_mdp_comp_bind(struct device *dev, struct device *master,
+			void *data)
+{
+	struct mtk_mdp_comp *comp = dev_get_drvdata(dev);
+	struct mtk_mdp_dev *mdp = data;
+
+	mtk_mdp_register_component(mdp, comp);
+
+	return 0;
+}
+
+static void mtk_mdp_comp_unbind(struct device *dev, struct device *master,
+			   void *data)
+{
+	struct mtk_mdp_dev *mdp = data;
+	struct mtk_mdp_comp *comp = dev_get_drvdata(dev);
+
+	mtk_mdp_unregister_component(mdp, comp);
+}
+
+static const struct component_ops mtk_mdp_component_ops = {
+	.bind   = mtk_mdp_comp_bind,
+	.unbind = mtk_mdp_comp_unbind,
+};
+
+int mtk_mdp_comp_init(struct mtk_mdp_comp *comp, struct device *dev)
 {
 	struct device_node *larb_node;
 	struct platform_device *larb_pdev;
 	int i;
+	struct device_node *node = dev->of_node;
+	enum mtk_mdp_comp_type comp_type =
+		 (enum mtk_mdp_comp_type)of_device_get_match_data(dev);
 
-	comp->dev_node = of_node_get(node);
-	comp->type = comp_type;
+	INIT_LIST_HEAD(&comp->node);
 
 	for (i = 0; i < ARRAY_SIZE(comp->clk); i++) {
 		comp->clk[i] = of_clk_get(node, i);
@@ -72,15 +140,15 @@ int mtk_mdp_comp_init(struct device *dev, struct device_node *node,
 		}
 
 		/* Only RDMA needs two clocks */
-		if (comp->type != MTK_MDP_RDMA)
+		if (comp_type != MTK_MDP_RDMA)
 			break;
 	}
 
 	/* Only DMA capable components need the LARB property */
 	comp->larb_dev = NULL;
-	if (comp->type != MTK_MDP_RDMA &&
-	    comp->type != MTK_MDP_WDMA &&
-	    comp->type != MTK_MDP_WROT)
+	if (comp_type != MTK_MDP_RDMA &&
+	    comp_type != MTK_MDP_WDMA &&
+	    comp_type != MTK_MDP_WROT)
 		return 0;
 
 	larb_node = of_parse_phandle(node, "mediatek,larb", 0);
@@ -103,7 +171,55 @@ int mtk_mdp_comp_init(struct device *dev, struct device_node *node,
 	return 0;
 }
 
-void mtk_mdp_comp_deinit(struct device *dev, struct mtk_mdp_comp *comp)
+static int mtk_mdp_comp_probe(struct platform_device *pdev)
 {
-	of_node_put(comp->dev_node);
+	struct device *dev = &pdev->dev;
+	struct device_node *vpu_node;
+	int status;
+	struct mtk_mdp_comp *comp;
+
+	vpu_node = of_parse_phandle(dev->of_node, "mediatek,vpu", 0);
+	if (vpu_node) {
+		of_node_put(vpu_node);
+		/*
+		 * The device tree node with a mediatek,vpu property is deemed
+		 * the MDP "master" device, we don't want to add a component
+		 * for it in this function because the initialization for the
+		 * master is done elsewhere.
+		 */
+		dev_info(dev, "vpu node found, not probing\n");
+		return -ENODEV;
+	}
+
+	comp = devm_kzalloc(dev, sizeof(*comp), GFP_KERNEL);
+	if (!comp)
+		return -ENOMEM;
+
+	status = mtk_mdp_comp_init(comp, dev);
+	if (status) {
+		dev_err(dev, "Failed to initialize component: %d\n", status);
+		return status;
+	}
+
+	dev_set_drvdata(dev, comp);
+
+	return component_add(dev, &mtk_mdp_component_ops);
 }
+
+static int mtk_mdp_comp_remove(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+
+	component_del(dev, &mtk_mdp_component_ops);
+	return 0;
+}
+
+struct platform_driver mtk_mdp_component_driver = {
+	.probe          = mtk_mdp_comp_probe,
+	.remove         = mtk_mdp_comp_remove,
+	.driver         = {
+		.name   = "mediatek-mdp-comp",
+		.owner  = THIS_MODULE,
+		.of_match_table = mtk_mdp_comp_driver_dt_match,
+	},
+};
