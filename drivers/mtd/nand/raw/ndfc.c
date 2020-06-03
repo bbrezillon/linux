feda56446e6a6279a4ec184a0464ea50ea7169d8
Boris Brezillon <boris.brezillon@collabora.com>
2020-06-03 22:49:39 +0200

    mtd: rawnand: ndfc: Implement exec_op()
    Implement exec_op() so we can get rid of the legacy interface
    implementation.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- drivers/mtd/nand/raw/ndfc.c
+++ drivers/mtd/nand/raw/ndfc.c
@@ -16,6 +16,8 @@
  *  Copyright 2008 PIKA Technologies
  *    Sean MacLennan <smaclennan@pikatech.com>
  */
+
+#include <linux/delay.h>
 #include <linux/module.h>
 #include <linux/mtd/rawnand.h>
 #include <linux/mtd/nand_ecc.h>
@@ -182,6 +184,113 @@ err:
 	return ret;
 }
 
+static void ndfc_data_in(struct nand_chip *chip,
+			 const struct nand_op_instr *instr)
+{
+	struct ndfc_controller *ndfc = nand_get_controller_data(chip);
+	unsigned int i;
+
+	if (!instr->ctx.data.force_8bit &&
+	    !(instr->ctx.data.len & 3) &&
+	    !((uintptr_t)instr->ctx.data.buf.in & 3)) {
+		u32 *in = instr->ctx.data.buf.in;
+
+		for (i = 0; i < instr->ctx.data.len / 4; i++)
+			in[i] = in_be32(ndfc->ndfcbase + NDFC_DATA);
+	} else {
+		ioread8_rep(ndfc->ndfcbase + NDFC_DATA,
+			    instr->ctx.data.buf.in,
+			    instr->ctx.data.len);
+	}
+}
+
+static void ndfc_data_out(struct nand_chip *chip,
+			  const struct nand_op_instr *instr)
+{
+	struct ndfc_controller *ndfc = nand_get_controller_data(chip);
+	unsigned int i;
+
+	if (!instr->ctx.data.force_8bit &&
+	    !(instr->ctx.data.len & 3) &&
+	    !((uintptr_t)instr->ctx.data.buf.out & 3)) {
+		const u32 *out = instr->ctx.data.buf.out;
+
+		for (i = 0; i < instr->ctx.data.len / 4; i++)
+			out_be32(ndfc->ndfcbase + NDFC_DATA, out[i]);
+	} else {
+		iowrite8_rep(ndfc->ndfcbase + NDFC_DATA,
+			     instr->ctx.data.buf.out,
+			     instr->ctx.data.len);
+	}
+}
+
+static int ndfc_exec_instr(struct nand_chip *chip,
+			   const struct nand_op_instr *instr)
+{
+	struct ndfc_controller *ndfc = nand_get_controller_data(chip);
+	unsigned int i;
+
+	switch (instr->type) {
+	case NAND_OP_CMD_INSTR:
+		writel(instr->ctx.cmd.opcode, ndfc->ndfcbase + NDFC_CMD);
+		return 0;
+
+	case NAND_OP_ADDR_INSTR:
+		for (i = 0; i < instr->ctx.addr.naddrs; i++)
+			writel(instr->ctx.addr.addrs[i], ndfc->ndfcbase + NDFC_ALE);
+		return 0;
+
+	case NAND_OP_DATA_IN_INSTR:
+		ndfc_data_in(chip, instr);
+		return 0;
+
+	case NAND_OP_DATA_OUT_INSTR:
+		ndfc_data_out(chip, instr);
+		return 0;
+
+	case NAND_OP_WAITRDY_INSTR:
+		return nand_poll(in_be32(ndfc->ndfcbase + NDFC_STAT) & NDFC_STAT_IS_READY,
+				 10, 10, instr->ctx.waitrdy.timeout_ms, true);
+
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int ndfc_exec_op(struct nand_chip *chip,
+			const struct nand_operation *op,
+			bool check_only)
+{
+	struct ndfc_controller *ndfc = nand_get_controller_data(chip);
+	unsigned int i;
+	int ret = 0;
+	u32 ccr;
+
+	if (check_only)
+		return 0;
+
+	ccr = in_be32(ndfc->ndfcbase + NDFC_CCR) & ~NDFC_CCR_BS_MASK;
+	out_be32(ndfc->ndfcbase + NDFC_CCR,
+	         ccr | NDFC_CCR_BS(op->cs + ndfc->chip_select));
+	for (i = 0; i < op->ninstrs; i++) {
+		ret = ndfc_exec_instr(chip, &op->instrs[i]);
+		if (ret)
+			break;
+
+		if (op->instrs[i].delay_ns)
+			ndelay(op->instrs[i].delay_ns);
+	}
+	out_be32(ndfc->ndfcbase + NDFC_CCR, ccr | NDFC_CCR_RESET_CE);
+
+	return ret;
+}
+
+static const struct nand_controller_ops ndfc_ops = {
+	.exec_op = ndfc_exec_op,
+};
+
 static int ndfc_probe(struct platform_device *ofdev)
 {
 	struct ndfc_controller *ndfc;
@@ -207,6 +316,7 @@ static int ndfc_probe(struct platform_device *ofdev)
 	ndfc->chip_select = cs;
 
 	nand_controller_init(&ndfc->ndfc_control);
+	ndfc->ndfc_control.ops = &ndfc_ops;
 	ndfc->ofdev = ofdev;
 	dev_set_drvdata(&ofdev->dev, ndfc);
 
