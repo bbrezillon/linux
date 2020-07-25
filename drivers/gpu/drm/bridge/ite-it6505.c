c6aecdf24fed7c1bc2547ce8f1145320de09615f
Boris Brezillon <boris.brezillon@collabora.com>
2020-07-25 08:45:58 +0200

    fixup! drm/bridge: add it6505 driver

--- drivers/gpu/drm/bridge/ite-it6505.c
+++ drivers/gpu/drm/bridge/ite-it6505.c
@@ -2538,7 +2538,6 @@ static void it6505_bridge_detach(struct drm_bridge *bridge)
 
 static enum drm_mode_status
 it6505_bridge_mode_valid(struct drm_bridge *bridge,
-			 const struct drm_display_info *info,
 			 const struct drm_display_mode *mode)
 {
 	struct it6505 *it6505 = bridge_to_it6505(bridge);
